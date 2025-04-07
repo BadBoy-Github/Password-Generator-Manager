913103c955b8c7cd01982f2c5f9b2ee58d234f50
Elayabarathi M V <elayabarathiedison@gmail.com>
2025-04-07 20:53:28 +0530

    changes

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 from tkinter import *
 from tkinter import messagebox
 from random import randint, choice, shuffle
@@ -6,7 +5,6 @@ import pyperclip
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
-#Password Generator Project
 def generate_password():
     letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 
                'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
@@ -27,17 +25,18 @@ def generate_password():
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
-def add_paddword():
+def add_password():
     website = website_entry.get()
     email = email_entry.get()
     password = password_entry.get()
     
     if len(website) == 0 or len(password) == 0 or len(email) == 0:
         messagebox.showwarning(title="Oops!!", message="Don't leave any field empty!")
-    
     else:
-        is_ok = messagebox.askokcancel(title="Check the information", message=f"These are the details entered: \nWebsite: {website} \nEmail: {email} 
-                                       \nPassword: {password} \nIs it ok to save?")
+        is_ok = messagebox.askokcancel(
+            title="Check the information", 
+            message=f"These are the details entered:\nWebsite: {website}\nEmail: {email}\nPassword: {password}\nIs it ok to save?"
+        )
         if is_ok:
             with open("data.txt", "a") as data_file:
                 data_file.write(f"{website} | {email} | {password}\n")
@@ -73,7 +72,7 @@ website_entry.focus()
 
 email_entry = Entry(width=54)
 email_entry.grid(row=2, column=1, columnspan=2)
-email_entry.insert(0, "elayabarathiedison@email.com")
+email_entry.insert(0, "example@email.com")
 
 password_entry = Entry(width=35)
 password_entry.grid(row=3, column=1)
@@ -82,8 +81,7 @@ password_entry.grid(row=3, column=1)
 generate_password_button = Button(text="Generate Password", command=generate_password)
 generate_password_button.grid(row=3, column=2)
 
-add_button = Button(text="Add", width=46,command=add_paddword)
+add_button = Button(text="Add", width=46, command=add_password)
 add_button.grid(row=4, column=1, columnspan=2)
-
 
 window.mainloop()
